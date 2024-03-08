0f88d7f2a899ade3a3ebf5c8cde6775d5298e3c0
Fernando Rojas <rojasfj@gmail.com>
2024-03-07 20:56:13 -0700

    agregamos carpetas e idioma

---
+++ 1er Semestre/Laboratorio 1/Python/Clase 3 - Tipos de datos/Practica3.py
@@ -0,0 +1,4 @@
+titulo = input("escribe el nombre del libro: ")
+autor = input("escribe el autor del libro: ")
+valor = int(input("ingrese el valor del libro: $ "))
+print((" los datos del libro son: ")+("el nombre es: "), titulo + ("el autor es : "),autor + ("con el costo de: $ "),valor) ## Corregir, esta mal pero no tanto
